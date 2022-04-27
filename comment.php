b70db26b504741f3f6a221d28c847a2465ed8273
Nissaq <2020470222@student.uitm.edu.my>
2022-04-27 09:05:50 +0800

    comment

--- comment.php
+++ comment.php
@@ -11,6 +11,7 @@
 // This is a comment
 //echo "X equals $x";
 $x =+ 10; //Increment $x by 10 echo $x;
+echo $x;
 /*This is a section of multiline comments that will not be interpreted */
 ?>
 </body>
